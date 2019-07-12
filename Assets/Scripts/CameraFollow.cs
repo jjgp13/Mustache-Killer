2e14d0ee677b583107b86fcc37558731705d4cda
Jose Galvez <jjgalper@gmail.com>
2019-07-12 18:06:05 -0500

    Initial commit
    Incluing some models and camera behaviour

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float followSpeed = 10f;
+
+    public Vector3 offsetCamera;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offsetCamera;
+        Vector3 smoothedPostion = Vector3.Lerp(transform.position, desiredPosition, followSpeed * Time.deltaTime);
+        transform.position = smoothedPostion;
+
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
