b9689512b66d0deb0c4fda09cba32ed13b98ad67
Jose Galvez <jjgalper@gmail.com>
2019-07-19 19:31:55 -0500

    Player Shoot

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField]
+    private float bulletSpeed;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.up * bulletSpeed * Time.deltaTime);
+        //rb.MovePosition(Vector3.up * bulletSpeed * Time.deltaTime);
+    }
+}
